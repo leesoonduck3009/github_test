dd080eb267e8fd03c53d9e1618813bb4a523235f
Phúc Bình Nguyễn <nguyen.phucbinh445@gmail.com>
2022-09-10 03:10:10 +0800

    Test C#

---
+++ lableGUI/lableGUI/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace lableGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Purple;
+            this.button1.FlatAppearance.BorderSize = 5;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(494, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hello";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 121);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 166);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.CausesValidation = false;
+            this.textBox2.Location = new System.Drawing.Point(372, 215);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = 'k';
+            this.textBox2.Size = new System.Drawing.Size(207, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ lableGUI/lableGUI/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lableGUI
+{
+
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadLb();
+            
+        }
+        void LoadLb()
+        {
+            Label lb = new Label();
+            lb.Text = "Hello Binh";
+            lb.Location = new Point(30, 20);
+            lb.Name = "Binh";
+            lb.BackColor = Color.Wheat;
+            lb.Click += Lb_Click;
+            this.Controls.Add((Label)lb);
+           
+        }
+
+        private void Lb_Click(object sender, EventArgs e)
+        {
+            Label temp= sender as Label;
+            temp.Text = "Hello moi nguoi";
+        }
+
+        void LoadBtt()
+        {
+            Random random = new Random();   
+            Button button=new Button();
+            button.Text = "Nhap";
+            button.Location = new Point(random.Next(0,this.Size.Width), random.Next(0,this.Size.Height));
+            button.Click += Button_Click;
+            this.Controls.Add((Button)button);
+            i++;
+        }
+
+        void LoadTexBox()
+        {
+
+        }
+        private void Button_Click(object sender, EventArgs e)
+        {
+           Button temp= sender as Button;
+            MessageBox.Show(temp.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoadBtt();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
