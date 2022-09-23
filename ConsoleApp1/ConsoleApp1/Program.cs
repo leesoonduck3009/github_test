627ddaf4d71a30e68d0ba0f21a4e8a71f34b6a97
Phúc Bình Nguyễn <nguyen.phucbinh445@gmail.com>
2022-09-23 09:21:04 +0700

    HELLO

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Shape
+    {
+        public abstract void Draw();
+        public abstract void Input();
+    }
+    class Rectangle : Shape
+    {
+        int length;
+        int width;
+        public override void Draw()
+        {
+            Console.WriteLine("Length: " );
+            Console.WriteLine("Width: " );
+        }
+        public override void Input()
+        {
+            length = int.Parse(Console.ReadLine());
+            width = int.Parse(Console.ReadLine());
+        }
+    }
+    class Triangle : Shape
+    {
+        int R;
+        
+        public override void Draw()
+        {
+            Console.WriteLine("This is triangle: " + R);
+        }
+        public override void Input()
+        {
+           R= int.Parse(Console.ReadLine());
+        }
+    }
+    class a
+    { 
+        public static void Nhap(Shape[] arr,ref int n)
+        {
+            Console.WriteLine("Nhap so phan tu: ");
+            n=int.Parse(Console.ReadLine());    
+           // arr=new Shape[n];
+            arr[0] = new Triangle();
+            arr[1] = new Rectangle();
+            arr[0].Input();
+            arr[1].Input();
+            arr[0].Draw();
+            arr[1].Draw();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] b=new Shape[2];
+            int n=2;
+            a.Nhap(b,ref n);
+            Console.ReadKey();
+        }
+    }
+}
